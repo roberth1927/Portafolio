3ab213858aa0d0db4ad50ebe5c0878484d8898b8
Roberth <45379245+roberth1927@users.noreply.github.com>
2023-08-17 11:55:36 -0500

    completed version 1.0.0

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,3 +1,18 @@
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+import { Navigation, Pagination } from 'swiper';
+
+
+
+import Image from "next/image";
+import { FaQuoteLeft } from 'react-icons/fa'
+
+
+
 // testimonial data
 const testimonialData = [
   {
@@ -23,8 +38,55 @@ const testimonialData = [
   },
 ];
 
-const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+const  TestimonialSlider = () => {
+  return (
+
+     <Swiper 
+      navigation={true}
+      pagination={{
+        clickable:true,
+      }}
+      modules={[Navigation, Pagination]}
+      className='h-[400px]'
+     >
+
+     {
+        testimonialData.map((person, index) => (
+          
+          <SwiperSlide key={index}>
+
+              <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
+                <div className='w-full max-w-[300px] flex flex-col xl:justify-center items-center realtive mx-auto xl:mx-0'>
+                   <div className='flex flex-col justify-center text-center'>
+
+                       <div>
+                         <Image src={person.image} width={100} height={100} alt='' />                          
+                       </div>
+
+                       <div className='text-lg'>{person.name}</div>
+                       <div className='text-[12px] uppercase front-extralight tracking-widest'>{person.position}</div>
+                   </div>
+                </div>
+
+                <div className='flex-1 flex flex-col justify-center 
+                                before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 
+                                xl:before:h-[200px] relative xl:pl-20'>
+
+                  <div className='mb-4'>
+                    <FaQuoteLeft  className='text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0'/></div>
+
+                  <div className='xl:text-lg text-center md:text-left'>
+                    {person.message}
+                  </div>
+
+                </div>
+                
+              </div>
+
+          </SwiperSlide>
+        ))}
+     </Swiper>
+  );
 };
 
 export default TestimonialSlider;
